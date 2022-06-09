575937863831b9ef3fcb601763e6331e54f8d340
Seynabou12 <dioneseynabou0@gmail.com>
2022-06-09 16:03:03 +0000

    installation de bootstrap et ajout du navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MusicComponent } from './music/music.component';
+import { LibraryComponent } from './library/library.component';
+import { MusicListComponent } from './music-list/music-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MusicComponent,
+    LibraryComponent,
+    MusicListComponent
   ],
   imports: [
     BrowserModule,
